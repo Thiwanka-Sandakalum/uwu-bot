d4241f3bd842be5bbeeead8eb438274f9208a331
thiwanka sandakalum <thiwanka2002sandakalum@gmail.com>
2023-11-21 01:57:39 +0530

    ..

--- controller/timetable_controler.js
+++ controller/timetable_controler.js
@@ -1,9 +1,10 @@
 
 function upcoming_lecture() {
     const time = new Date()
-    console.log(time.getHours());
+    console.log(time.getHours(),time.getMinutes());
 
-    // 
+    //find time slote
+
 }
 
 upcoming_lecture()
