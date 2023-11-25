9b437668a78d1ea3aa324157dc0f8abcd283d180
nishansanjuka <nipunanishan47@gmail.com>
2023-11-26 01:57:45 +0530

    amboo meka wenas karannepa

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 // db.js
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./data.db');
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database('data.sqlite');
 
 module.exports = db;
