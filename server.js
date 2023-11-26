8b178c2de79753e1c758b89ee8c34bc68b080df6
nishansanjuka <nipunanishan47@gmail.com>
2023-11-26 16:36:02 +0530

    hey

--- server.js
+++ server.js
@@ -5,25 +5,24 @@ const api = require('./api/index');
 const chatID = '1356513283';
 
 
-cron.schedule('0 7,8,10,12 * * 1-5', async () => {
+cron.schedule('*/5 * * * * *', async () => {
     try {
         const timetableData = await upcoming_lecture();
 
         if (timetableData !== null) {
-            let responseMessage = 
-                                `**Next Lecture**\n
-**${timetableData.Day}**
-**${timetableData.TimeStart} - ${timetableData.TimeEnd}**\n
-- **📚 ${timetableData.CourseName}**
-- **👨‍🏫 ${timetableData.LecturerName}**
-- **🏫 ${timetableData.Location}**`;
+            let responseMessage = `Next Lecture\n
+${timetableData.Day}\n
+${timetableData.TimeStart} - ${timetableData.TimeEnd}\n
+- 📚 ${timetableData.CourseName}
+- 👨‍🏫 ${timetableData.LecturerName}
+- 🏫 ${timetableData.Location}`;
             bot.sendMessage(chatID, responseMessage, { parse_mode: 'HTML' });
         } else {
-            bot.sendMessage(chatID, "There are no any lectures for today", { parse_mode: 'Markdown' });
+            // bot.sendMessage(chatID, "There are no any lectures for today", { parse_mode: 'HTML' });
         }
     } catch (error) {
         logger.error(error);
-        bot.sendMessage(chatID, "Error fetching timetable data.");
+        bot.sendMessage(chatID, "The Bot is sleeping now");
     }
 }, {
     timezone: 'Asia/Colombo'
